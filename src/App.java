40dac250db632c7d9c336f720b4b86b8df9a759a
AJuanesMegma <ajuan@itpfp.com>
2023-12-12 14:38:21 +0100

    prueba

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        String[] preguntas = new String[15];
+        String[] preguntasssss = new String[15];
     }
 }
