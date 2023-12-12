c9fa174cec5c9dce68c1b467ce5bd92583fedcf0
AJuanesMegma <ajuan@itpfp.com>
2023-12-12 14:41:19 +0100

    Prueba2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
-        String[] preguntasssss = new String[15];
+        Scanner sc = new Scanner("System.in");
+        String[] preguntas = new String[15];
+        String[] respuestas = new String[15];
+        preguntas[0] = "¿España ganó el mundial en el 2010?";           respuestas[0] = "Si";
+        preguntas[1] = "¿Gibraltar pertenece a la Union Europea?";      respuestas[0] = "No";
+        preguntas[2] = "¿Marruecos se merecia ganar el mundial?";       respuestas[0] = "No";
+        preguntas[3] = "¿Me gustan los caballos?";                      respuestas[0] = "Si";
+        preguntas[4] = "¿Tengo el carnet de conducir?";                 respuestas[0] = "No";
+        preguntas[5] = "¿?";respuestas[0] = "";
+        preguntas[6] = "¿?";respuestas[0] = "";
+        preguntas[7] = "¿?";respuestas[0] = "";
+        preguntas[8] = "¿?";respuestas[0] = "";
+        preguntas[9] = "¿?";respuestas[0] = "";
+        preguntas[10] = "¿?";respuestas[0] = "";
+        preguntas[11] = "¿?";respuestas[0] = "";
+        preguntas[12] = "¿?";respuestas[0] = "";
+        preguntas[13] = "¿?";respuestas[0] = "";
+        preguntas[14] = "¿?";respuestas[0] = "";
+
     }
 }
